56a88f5a71981d0a9099ec83469c8975bf59af33
Fumaz <mail+github@fumaz.dev>
2021-09-08 19:19:37 +0200

    it do be a supplier

--- src/main/java/dev/fumaz/commons/bukkit/interfaces/Item.java
+++ src/main/java/dev/fumaz/commons/bukkit/interfaces/Item.java
@@ -3,13 +3,20 @@ package dev.fumaz.commons.bukkit.interfaces;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Supplier;
+
 /**
  * A class that has an {@link ItemStack}
  */
-public interface Item {
+public interface Item extends Supplier<ItemStack> {
 
     @NotNull
     ItemStack getItemStack();
+
+    @Override
+    default ItemStack get() {
+       return getItemStack();
+    }
 
     // TODO Implement isItem, hasItem, etc methods
 
