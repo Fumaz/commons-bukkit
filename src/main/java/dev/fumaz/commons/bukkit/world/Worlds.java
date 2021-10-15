9c4fc2bca9d5ead6f825950a13d13825255032a6
Fumaz <mail@fumaz.dev>
2021-10-15 17:38:19 +0200

    1.11: getHighestSolidBlockAt()

--- src/main/java/dev/fumaz/commons/bukkit/world/Worlds.java
+++ src/main/java/dev/fumaz/commons/bukkit/world/Worlds.java
@@ -1,8 +1,22 @@
 package dev.fumaz.commons.bukkit.world;
 
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
 public final class Worlds {
 
     private Worlds() {
+    }
+
+    public static Block getHighestSolidBlockAt(World world, int x, int z) {
+        Block highest = world.getHighestBlockAt(x, z);
+
+        while (!highest.isSolid()) {
+            highest = highest.getRelative(BlockFace.DOWN);
+        }
+
+        return highest;
     }
 
 }
